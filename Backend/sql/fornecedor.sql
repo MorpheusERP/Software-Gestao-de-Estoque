426c35d7007cc06a216d34875393194afe053ae5
IuryHilario <iury0hilario@gmail.com>
2024-10-30 13:38:07 -0300

    Adicionando a tabela do banco de dados, definindo Primary Key (PK) e Foreign Key (FK),  fazendo as relações entre as tabelas e excluindo os arquivos .sql que eram temporarios

--- Backend/sql/fornecedor.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE fornecedores (
-    id_Fornecedor INT(11) PRIMARY KEY,
-    razao_Social VARCHAR(120) NOT NULL,
-    nome_Fornecedor VARCHAR(120) NOT NULL,
-    apelido VARCHAR(120) NOT NULL,
-    grupo VARCHAR(120) NOT NULL,
-    sub_Grupo VARCHAR(120) NOT NULL,
-    observacao VARCHAR(120) NOT NULL
-);
